def46fc9221d178a69f81968621b5fd4fa98f794
simegnew <simegnew202.m@gmail.com>
2023-03-21 04:22:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+#include "main.h"
 
 
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+/* function returning no value */
 	void print_alphabet(void)
 	{
 		char letter;
