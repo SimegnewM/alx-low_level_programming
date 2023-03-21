012af274cabf4dbab3916db1cb246698bfdf7f1b
simegnew <simegnew202.m@gmail.com>
2023-03-20 22:58:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -chek the code
+ *
+ * Reteurn; Alwaysn0.
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z' letter++)
+		_putchar(letter);
+	
+	_putchar('\n');
+}
