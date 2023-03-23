452b0bfb29021b199fcaa1d7b4c7922c7c3b33e3
simegnew <simegnew202.m@gmail.com>
2023-03-22 23:15:10 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
         int i;
 
-        printf("Infinite loop incoming :\n");
+        printf("Infinite loop incoming :(\n"); /*this prints th output infinite loop is comming*/
 
         i = 0;
 
         while (i < 10)
         {
-                putchar(i);
+                putchar(i);/*this prints the char equivalent of letters*/
         }
 
         printf("Infinite loop avoided! \\o/\n");
